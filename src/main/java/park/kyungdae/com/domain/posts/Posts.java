52ebdd731e7d97640b7439b9ceeacc89e610ae36
Kyungdae Park <imdongle123@gmail.com>
2022-05-11 23:35:49 +0900

    5/11 JPA 사용 및 MYSQL버전 쿼리 로그 출력 (P.100)

---
+++ src/main/java/park/kyungdae/com/domain/posts/Posts.java
@@ -0,0 +1,34 @@
+package park.kyungdae.com.domain.posts;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter // 클래스 내 모든 필드의 Getter 메소드 자동생성
+
+@NoArgsConstructor // 기본 생성자 자동 추가
+@Entity // 테이블과 링크될 클래스임을 알려줌 : 'posts' 테이블을 만듬
+// @Entity 에서는 Setter 메소드를 절대 만들지 않음 (나중에 기능 바꿀때 복잡해짐)
+public class Posts {
+
+    @Id // 테이블의 PK(Primary Key) 필드를 나타냄.
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // auto_increment로 pk 필드를 만들거임
+    private Long id;
+
+    @Column(length=500, nullable = false) // 원래 안해도 됨(기본값이 column임), 근데 VARCHAR(255)의 길이를 500으로 늘리고 싶어서 사용
+    private String title;
+
+    @Column(columnDefinition = "TEXT", nullable = false) // 타입을 TEXT로 변경하고 싶음
+    private String content;
+
+    private String author;
+
+    @Builder
+    public Posts(String title, String content, String author){
+        this.title = title;
+        this.content = content;
+        this.author = author;
+    }
+}
---
+++ src/main/java/park/kyungdae/com/domain/posts/PostsRepository.java
@@ -0,0 +1,12 @@
+// Posts클래스로 Database를 접근하게 해줄 JpaRepository 생성
+// 이 레포지토리는 Posts클래스 즉, Entity 클래스와 함께 위치해야 함
+// 나중에 규모가 커져 도메인별로 프로젝트를 분리해야 한다면, 같이 움직여야 하기 때문에 하나의 패키지 內에서 관리함
+
+package park.kyungdae.com.domain.posts;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostsRepository extends JpaRepository<Posts,Long> {
+    //JpaRepository<Entity클래스, PK타입>
+
+}
